1044a9870bc22820d958c4d81f4f227a16eb9ddc
victor alvarez <victoralvarezsaucedo@gmail.com>
2021-08-14 18:38:19 -0600

    update state sale

--- app/controllers/Pos.php
+++ app/controllers/Pos.php
@@ -502,15 +502,12 @@ class Pos extends MY_Controller
             $round_total = $this->tec->roundNumber($grand_total, $this->Settings->rounding);
             $rounding    = $this->tec->formatDecimal(($round_total - $grand_total));
             
-            if (!$eid) {
-                $status = 'due';
-                if ($this->tec->formatDecimal($round_total) <= $this->tec->formatDecimal($paid)) {
-                    $status = 'paid';
-                } elseif ($this->tec->formatDecimal($round_total) > $this->tec->formatDecimal($paid) && $paid > 0) {
-                    $status = 'partial';
-                }
+            $status = 'due';
+            if ($this->tec->formatDecimal($round_total) <= $this->tec->formatDecimal($paid)) {
+                $status = 'paid';
+            } elseif ($this->tec->formatDecimal($round_total) > $this->tec->formatDecimal($paid) && $paid > 0) {
+                $status = 'partial';
             }
-
 
             $totalpagos = $this->input->post('total_pagos');
 
